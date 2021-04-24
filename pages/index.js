62148a85e271dfca93143d91e62c127ac666a842
Julio Mayorga <juliomayorgaubiria@gmail.com>
2021-04-24 02:17:24 -0300

    comit encima del primer commit remoto

---
+++ pages/index.js
@@ -0,0 +1,81 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.css';
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>;
+
+export default function Home() {
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Julio Mayorga Ubiría</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className={styles.main}>
+				<div className={styles.header}>
+					<div className={styles.logo}>JULIOMU_</div>
+					<div className={styles.headerIcons}>
+						<svg
+							width="24"
+							height="24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M20.625 2.625H3.375a.75.75 0 00-.75.75v17.25c0 .415.335.75.75.75h17.25a.75.75 0 00.75-.75V3.375a.75.75 0 00-.75-.75zm-6.598 5.6h3.244v.79h-3.244v-.79zM12 14.737a2.1 2.1 0 01-.633.726 2.784 2.784 0 01-.91.415c-.34.091-.692.136-1.045.133h-3.88V8.018h3.773c.382 0 .729.035 1.045.1.314.066.581.179.806.331.223.153.399.356.523.61.122.25.185.564.185.937 0 .403-.091.736-.274 1.006-.185.27-.453.487-.816.658.495.14.858.392 1.097.743.244.356.363.783.363 1.284 0 .408-.077.758-.234 1.05zm6.534-1.238h-4.18c0 .455.156.89.393 1.125.239.232.581.35 1.029.35.323 0 .597-.083.832-.245.232-.161.372-.332.424-.51h1.402c-.225.696-.568 1.192-1.032 1.492-.459.3-1.021.45-1.675.45a3.38 3.38 0 01-1.236-.217 2.626 2.626 0 01-.935-.622 2.839 2.839 0 01-.588-.965 3.513 3.513 0 01-.209-1.233c0-.434.073-.837.214-1.212.27-.729.83-1.312 1.544-1.615.382-.159.792-.239 1.207-.234.492 0 .919.094 1.29.286.356.184.665.447.904.769.237.32.403.687.509 1.099.1.405.136.832.107 1.282zm-2.859-2.24c-.253 0-.466.044-.63.13-.165.087-.3.195-.404.32-.098.12-.17.258-.213.407-.037.124-.063.251-.073.38h2.59c-.037-.406-.178-.706-.365-.916-.197-.21-.514-.322-.905-.322zm-5.824-.237c.204-.148.302-.392.302-.727a.943.943 0 00-.096-.46.739.739 0 00-.272-.278 1.05 1.05 0 00-.389-.14 2.27 2.27 0 00-.452-.04H7.296v1.867H9.08c.307.003.567-.072.77-.222zm.276 1.687c-.23-.176-.536-.262-.918-.262H7.29v2.203h1.88c.176 0 .338-.017.495-.05.157-.032.297-.089.417-.168a.845.845 0 00.288-.319c.07-.136.106-.31.106-.518 0-.415-.118-.708-.35-.886z"
+								fill="#FCFCFC"
+							/>
+						</svg>
+
+						<lottie-player
+							src="https://assets4.lottiefiles.com/packages/lf20_nwvalyrc.json"
+							background="transparent"
+							speed="1"
+							style={{ width: '24px', height: '24px' }}
+							hover
+						></lottie-player>
+
+						<svg
+							width="24"
+							height="24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M12 8.876A3.13 3.13 0 008.876 12 3.13 3.13 0 0012 15.124 3.13 3.13 0 0015.124 12 3.13 3.13 0 0012 8.876zM21.37 12c0-1.294.012-2.576-.06-3.867-.073-1.5-.415-2.831-1.512-3.928-1.1-1.1-2.428-1.44-3.928-1.512-1.294-.073-2.576-.061-3.867-.061-1.294 0-2.576-.012-3.868.061-1.5.073-2.831.415-3.928 1.512-1.1 1.099-1.439 2.428-1.512 3.928-.072 1.294-.06 2.576-.06 3.867s-.012 2.576.06 3.867c.073 1.5.415 2.831 1.512 3.928 1.1 1.1 2.428 1.44 3.928 1.512 1.294.073 2.576.061 3.867.061 1.294 0 2.576.012 3.868-.061 1.5-.073 2.83-.415 3.928-1.512 1.099-1.099 1.439-2.428 1.512-3.928.075-1.291.06-2.573.06-3.867zM12 16.807A4.8 4.8 0 017.193 12 4.8 4.8 0 0112 7.193 4.8 4.8 0 0116.807 12 4.8 4.8 0 0112 16.807zm5.004-8.688a1.121 1.121 0 01-1.123-1.123c0-.621.502-1.123 1.123-1.123a1.121 1.121 0 01.43 2.16 1.121 1.121 0 01-.43.086z"
+								fill="#FCFCFC"
+							/>
+						</svg>
+						<svg
+							width="24"
+							height="24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M20.625 2.625H3.375a.75.75 0 00-.75.75v17.25c0 .415.335.75.75.75h17.25a.75.75 0 00.75-.75V3.375a.75.75 0 00-.75-.75zM8.187 18.602H5.405V9.654h2.782v8.948zM6.797 8.43a1.612 1.612 0 110-3.224 1.612 1.612 0 010 3.224zm11.805 10.172h-2.78V14.25c0-1.038-.018-2.372-1.445-2.372-1.447 0-1.67 1.13-1.67 2.297v4.427H9.93V9.654h2.668v1.223h.037c.37-.703 1.277-1.446 2.632-1.446 2.817 0 3.335 1.854 3.335 4.264v4.907z"
+								fill="#FCFCFC"
+							/>
+						</svg>
+					</div>
+				</div>
+				<div className={styles.body}>
+					<div className={styles.title}>Hi, I'm Julio Mayorga</div>
+					<div className={styles.subtitle}>UX Engineer at HOLOS Technology</div>
+				</div>
+				<div></div>
+				<div className={styles.presentation}>
+					I'm an experienced product designer with three years of experience
+					conceptualizing and crafting digital products. I work end to end from
+					ideation to front-end development, provinding solid outcomes while
+					working in product strategy, facilitation and prototyping. Skilled and
+					creative establishing products, content and experience strategy. I
+					proud myself of being a long term thinker which always takes care of
+					the details in the process. Passionate about finding ways to make a
+					product clear and easy to use in order to achieve its goals.
+					Proficient delivering user flows, information architecture, wireframes
+					and prototypes.
+				</div>
+			</main>
+		</div>
+	);
+}
